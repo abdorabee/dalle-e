efcaea09b3b36938de9435c688311faf0e4491de
abdorabee <abdorabee1134@gmail.com>
2023-02-01 16:52:49 +0200

    connected to MongoDb

--- server/index.js
+++ server/index.js
@@ -2,20 +2,30 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
+import connectDB from "./mongodb/connect.js";
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({
+    limit: "50mb"
+}));
 
 app.get("/", async (req, res) => {
-  res.send("hello from the server ");
+    res.send("hello from the server ");
 });
 
 const startServer = async () => {
-  app.listen(8080, () =>
-    console.log("Server is running on port http://localhost:8080 ")
-  );
+
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, () =>
+            console.log("Server is running on port http://localhost:8080 ")
+        );
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 startServer();
